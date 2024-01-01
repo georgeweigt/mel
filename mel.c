d66acc5ca8229c4181dd1494e02a1540f580976b
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-12-31 19:28:57 -0700

    update

--- mel.c
+++ mel.c
@@ -1,6 +1,4 @@
-/* github.com/georgeweigt/story-of-mel/mel.c
-
-August 13, 2020
+/* August 13, 2020
 
 This program runs Mel Kaye's blackjack program bkjck.tx by simulating an LGP-30 computer.
 
@@ -35,6 +33,32 @@ Card?
                               9 - d
                               K - s
                blackjack      bust           score = -$3.00
+
+BSD 2-Clause License
+
+Copyright (c) 2020, George Weigt
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -168,7 +192,7 @@ udiv(uint32_t x, uint32_t y)
 
 int ptab[128] = {
 	0,	0,	'Z',	'z',	')',	'0',	' ',	' ',
-	0,	0,	'B',	'b',	'L',	'l',	'_',	'-',
+	0,	0,	'B',	'b',	'L',	'1',	'_',	'-',
 	0,	0,	'Y',	'y',	'*',	'2',	'=',	'+',
 	0,	0,	'R',	'r',	'"',	'3',	':',	';',
 	'\n',	'\n',	'I',	'i',	0,	'4',	'?',	'/',
